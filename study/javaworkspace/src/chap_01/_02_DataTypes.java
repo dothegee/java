1559251092caf7cb58562175cd3d2c8f22f54bbc
dothegee <sjuinobat@gmail.com>
2023-07-18 15:07:44 +0900

    자료형

---
+++ study/javaworkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,26 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        // 문자열
+        System.out.println("Hello world");
+        System.out.println("안녕");
+        // 숫자 자료형
+        System.out.println(1203);
+        System.out.println(3.140);
+        // bool
+        System.out.println(true);
+        System.out.println(false);
+        // 계산
+        System.out.println(123 + 345);
+    }
+}
+
+// psvm 을 치면
+// public static void main(String[] args) {
+        
+//     }
+// 자동으로 만들어짐
+// sout 을 치면
+//  System.out.println();
+// 자동 완성
