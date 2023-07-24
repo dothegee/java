5010ff566a934490b0c52dd757ed5920c87f2732
dothegee <sjuinobat@gmail.com>
2023-07-24 13:21:05 +0900

    다차원 배열

---
+++ study/javaworkspace/src/chap_05/_03_MultiArray.java
@@ -0,0 +1,9 @@
+package chap_05;
+
+public class _03_MultiArray {
+    public static void main(String[] args) {
+        String[][] seat = {{"A1","A2","A3"},{"B1","B2","B3"},{"C1","C2","C3"}};
+    // A3에 접근하려면?
+    System.out.println(seat[0][2]);
+    }
+}
