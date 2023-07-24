824874281b7e96c548f592d85eb543b47cbba6e9
dothegee <sjuinobat@gmail.com>
2023-07-24 13:27:29 +0900

    ASCII code

---
+++ study/javaworkspace/src/chap_05/_05_ASCII.java
@@ -0,0 +1,13 @@
+package chap_05;
+
+public class _05_ASCII {
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println(c);
+        System.out.println((int)c);
+        c++;
+        System.out.println(c);
+        int a = 65;
+        System.out.println((char)a);
+    }
+}
